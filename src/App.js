ef04a0106190c6d8e37cde3c3a359341b52c6841
Vishal Rathod <vrathod591@gmail.com>
2021-11-29 14:49:57 +0530

    Basic working structure created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Home from "./Components/Home/Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import "./home.css";
+import Navbar from "../Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <div id="container">
+      <h1>Book My Show</h1>
+    </div>
+  );
+}
